eea134cb7baedfa24e421cd9d9b01656376dc3a7
pslcn <gregorsrussellcobb@gmail.com>
2022-12-17 14:04:26 +0000

    clunky, but getting there

--- fingerprint.py
+++ fingerprint.py
@@ -8,22 +8,38 @@ from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 import torch.nn.functional as F
 
+# def batch_alter(batch):
+#     occurrences = batch_size // 2
+#     X, y = batch    
+#     focus = random.choice(y)
+#     y = torch.tensor([[1 if l == focus else 0] for l in y])
+#     X[occurrences:] = torch.squeeze(X[np.where(y.numpy())[0]])
+#     y[occurrences:] = torch.tensor([1])
+#     return (X, y)
+ 
 class Fingerprints(Dataset):
-    def __init__(self, finger, img_dim=(416, 416)):
+    def __init__(self, finger, focus, img_dim=(416, 416)):
         self.img_dim = img_dim
         self.data = []
         finger = finger.split(' ')
         for img_path in glob.glob(DATASET_URL + f'/*{finger[0].capitalize()}_{"_".join([e for e in finger[1:]])}.BMP'):
             img_id = int((img_path.split('/')[-1]).split('_')[0])
-            self.data.append([img_path, img_id])
+            if img_id == focus: 
+                self.focus = img_path
+                self.data.append([img_path, 1])
+            else:
+                self.data.append([img_path, 0])
+        for o in range(len(self.data) // 2):
+            self.data.append([self.focus, 1])
+        random.shuffle(self.data)
 
     def __len__(self): 
         return len(self.data)
 
     def __getitem__(self, idx):
-        img_path, img_id = self.data[idx]
+        img_path, isfocus = self.data[idx]
         img = torch.from_numpy(cv2.resize(cv2.imread(img_path), self.img_dim))
-        return img.permute(2, 0, 1).float(), img_id
+        return img.permute(2, 0, 1).float(), torch.tensor([isfocus])
 
 class ExampleImageNet(nn.Module):
     def __init__(self):
@@ -38,59 +54,54 @@ class ExampleImageNet(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         x = self.s(x)
         return x
 
-DATASET_URL = 'data/socofing/real'
-
-fingerprints = Fingerprints('left index finger')
-batch_size = 100 
-trainset = DataLoader(fingerprints, batch_size=batch_size, shuffle=True)
-
 def train(dataloader, network, loss_fn, optimiser):
     for epoch in range(1):
         running_loss = 0.0
-        for i, (X, y) in enumerate(dataloader):
-            X, y = X, y 
-            fid = random.choice(y) 
-            y = torch.tensor([[1 if l == fid else 0] for l in y])
+        for i, batch in enumerate(dataloader):
+            X, y = batch
             optimiser.zero_grad()
             pred = network(X)
             loss = loss_fn(pred, y.float())
             loss.backward()
             optimiser.step()
             running_loss += loss.item()
-            print(f'[{i + 1} loss: {running_loss}]')
+            print(f'{i + 1} loss: {running_loss}')
             running_loss = 0.0
 
 def test(testset, model, loss_fn):
-    size = len(testset.dataset)
-    batches = len(testset)
-    test_loss, correct = 0, 0
+    correct = 0
     with torch.no_grad():
-        for fid in range(1):
-            for X, y in testset:
-                y = torch.tensor([[1 if l == torch.tensor([fid]) else 0] for l in y]) 
-                pred = model(X)
-                test_loss += loss_fn(pred, y).item()
-                # correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-            test_loss /= batches
-            # correct /= size
-            # print(f'accuracy: {(100 * correct):>0.1f}%, avg loss: {test_loss:>8f}')
+        for data in testset:
+            X, y = data
+            pred = model(X)
+            correct += (pred == y).sum().item()
+    print(f'accuracy: {100 * correct // len(testset)}%')
+
+DATASET_URL = 'data/socofing/real'
+GENERIC_MODEL_URL = 'models/generic.pth'
+
+batch_size = 30 
 
 net = ExampleImageNet()
 loss_fn = nn.MSELoss() 
 optimiser = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-MODEL_PATH = 'models/net.pth'
-
+trainset = DataLoader(
+        Fingerprints('left index finger', random.randint(1, 600)), 
+        batch_size=batch_size, shuffle=True)
+net.load_state_dict(torch.load(GENERIC_MODEL_URL))
 train(trainset, net, loss_fn, optimiser)
-# torch.save(net.state_dict(), MODEL_PATH)
+torch.save(net.state_dict(), GENERIC_MODEL_URL)
 
-# testset = trainset
-# net.load_state_dict(torch.load(MODEL_PATH))
+# testset = DataLoader(
+#         Fingerprints('left index finger', 100),
+#         shuffle=True)
+# net.load_state_dict(torch.load(GENERIC_MODEL_URL))
 # test(testset, net, loss_fn)
