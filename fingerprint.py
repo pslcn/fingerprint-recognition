b703a59a5b503e0097c524495d200007d55d6881
pslcn <gregorsrussellcobb@gmail.com>
2022-12-17 08:11:17 +0000

    quick cleanup

--- fingerprint.py
+++ fingerprint.py
@@ -1,42 +1,33 @@
 import glob
 import cv2
 import os
-import imageio.v2 as imageio
 import numpy as np
 import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms import ToTensor
 import torch.optim as optim
 import torch.nn.functional as F
 
 DATASET_URL = 'data/socofing/real'
 
-# def get_img(name):
-#     return np.asarray(imageio.imread(os.path.join(DATASET_URL, name)))
-# fp = get_img('100__M_Left_index_finger.BMP')
-
 class Fingerprints(Dataset):
-    def __init__(self):
+    def __init__(self, finger, img_dim=(416, 416)):
+        self.img_dim = img_dim
         self.data = []
-        for img_path in glob.glob(DATASET_URL + '/*.BMP'):
-            img_id = int((img_path.split('_')[0]).split('/')[-1])
+        finger = finger.split(' ')
+        for img_path in glob.glob(DATASET_URL + f'/*{finger[0].capitalize()}_{"_".join([e for e in finger[1:]])}.BMP'):
+            img_id = int((img_path.split('/')[-1]).split('_')[0])
             self.data.append([img_path, img_id])
-        self.img_dim = (416, 416)
 
-    def __len__(self):
+    def __len__(self): 
         return len(self.data)
 
     def __getitem__(self, idx):
         img_path, img_id = self.data[idx]
         img = torch.from_numpy(cv2.resize(cv2.imread(img_path), self.img_dim))
-        img = img.permute(2, 0, 1)
-        # img_id = torch.tensor([img_id])
-        return img.float(), img_id
+        return img.permute(2, 0, 1).float(), torch.tensor([img_id])
 
-fingerprints = Fingerprints()
-print('The shape of tensor for 50th image in train dataset: ',fingerprints[49][0].shape)
-print('The label for 50th image in train dataset: ',fingerprints[49][1])
+fingerprints = Fingerprints('left index finger')
 
 batch_size = 4
 trainset = DataLoader(fingerprints, batch_size=batch_size, shuffle=True)
@@ -57,7 +48,6 @@ class ExampleImageNet(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        x = torch.reshape(x, (-1,)) # Reshape [4, 1] into [4]
         return x
 
 net = ExampleImageNet()
