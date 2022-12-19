adb0d620f7d32c27dedef173cdadd6d557587cab
pslcn <gregorsrussellcobb@gmail.com>
2022-12-19 16:46:23 +0000

    cleanup

--- fingerprint.py
+++ fingerprint.py
@@ -8,24 +8,22 @@ from torch import nn
 from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 import torch.nn.functional as F
-from torchvision import models
 
-class Fingerprints(Dataset):
+class TestFingerprints(Dataset):
     def __init__(self, finger, focus, img_dim=(416, 416)):
         self.img_dim = img_dim
-        self.images = []
-        self.labels = []
+        self.images, self.labels = [], []
         finger = finger.split(' ')
         for img_path in glob.glob(DATASET_URL + f'/*{finger[0].capitalize()}_{"_".join([e for e in finger[1:]])}.BMP'):
             img_id = int((img_path.split('/')[-1]).split('_')[0])
-            self.images.append(torch.from_numpy(cv2.resize(cv2.imread(img_path, cv2.IMREAD_GRAYSCALE), self.img_dim)[np.newaxis, :, :]).float())
+            self.images.append(cv2.resize(cv2.imread(img_path, cv2.IMREAD_GRAYSCALE), self.img_dim)[np.newaxis, :, :])
             self.labels.append(1 if img_id == focus else 0)
         focus = self.images[int(np.where(self.labels)[0])]
         for o in range(len(self.labels) // 2):
             self.images.append(focus)
             self.labels.append(1)
 
-    def __len__(self): 
+    def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, idx):
@@ -34,10 +32,9 @@ class Fingerprints(Dataset):
 class ImageProcess:
     @staticmethod
     def morph_op(img):
-        iterations = 2
-        ret = cv2.erode(img.numpy(), np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]]).astype('uint8'), iterations=iterations)
-        ret = cv2.dilate(img.numpy(), np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]]).astype('uint8'), iterations=iterations)
-        return ret
+        cross = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]]).astype('uint8')
+        ret = cv2.erode(img.numpy(), cross, iterations=1)
+        return cv2.dilate(ret, cross, iterations=2)
 
     @staticmethod
     def g_filter(img):
@@ -87,8 +84,8 @@ def show_transform(img1, img2):
 class FingerprintModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(1, out_channels=6, kernel_size=5)
+        self.pool = nn.MaxPool2d(2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 101 * 101, 120)
         self.fc2 = nn.Linear(120, 84)
@@ -96,28 +93,31 @@ class FingerprintModel(nn.Module):
         self.s = nn.Sigmoid()
 
     def forward(self, x):
-        imgplot = plt.imshow(x[0][0].numpy().astype('uint8'), cmap='gray')
-        plt.show()
+        # imgplot = plt.imshow(x[0][0].numpy().astype('uint8'), cmap='gray')
+        # plt.show()
 
         # Morphological operations
-        x = np.array([ImageProcess.morph_op(img) for img in x])
-        imgplot = plt.imshow(x[0][0].astype('uint8'), cmap='gray')
-        plt.show()
+        # x = np.array([ImageProcess.morph_op(img) for img in x])
+        x = [ImageProcess.morph_op(img) for img in x]
+        # imgplot = plt.imshow(x[0][0].astype('uint8'), cmap='gray')
+        # plt.show()
 
         # Gabor filtering for image enhancement
-        x = np.array([ImageProcess.g_filter(img) for img in x])
-        imgplot = plt.imshow(x[0][0].astype('uint8'), cmap='gray')
-        plt.show()
+        # x = np.array([ImageProcess.g_filter(img) for img in x])
+        x = [ImageProcess.g_filter(img) for img in x]
+        # imgplot = plt.imshow(x[0][0].astype('uint8'), cmap='gray')
+        # plt.show()
 
         # Feature extraction
-        features = np.array([ImageProcess.extract_features(img[0]) for img in x])
+        # features = np.array([ImageProcess.extract_features(img[0]) for img in x])
 
-        # Matching image 
+        # Matching image
         # focus_features = ImageProcess.extract_features(focus_img)
         # imgplot = plt.imshow(torch.squeeze(torch.tensor(x[ImageProcess.feature_match(focus_features, features)])).numpy().astype('uint8'), cmap='gray')
         # plt.show()
 
-        x = torch.tensor(x)
+        x = np.array(x)
+        x = torch.from_numpy(x).float()
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = torch.flatten(x, 1)
@@ -127,9 +127,18 @@ class FingerprintModel(nn.Module):
         x = self.s(x)
         return x
 
-def train(dataloader, network, loss_fn, optimiser):
-    for epoch in range(1):
-        running_loss = 0.0
+DATASET_URL = 'data/socofing/real'
+GENERIC_MODEL_URL = 'models/generic.pth'
+
+net = FingerprintModel()
+loss_fn = nn.MSELoss() 
+optimiser = optim.SGD(net.parameters(), lr=0.01)
+
+batch_size = 30 
+trainset = DataLoader(TestFingerprints('left index finger', random.randint(1, 600)), batch_size=batch_size, shuffle=True)
+
+def train(epochs, dataloader, network, loss_fn, optimiser):
+    for epoch in range(epochs):
         for i, batch in enumerate(dataloader):
             X, y = batch
             optimiser.zero_grad()
@@ -137,20 +146,8 @@ def train(dataloader, network, loss_fn, optimiser):
             loss = loss_fn(pred, y.float())
             loss.backward()
             optimiser.step()
-            running_loss += loss.item()
-            print(f'{i + 1} loss: {running_loss}')
-            running_loss = 0.0
+            print(f'{i + 1} loss: {loss.item()}')
 
-DATASET_URL = 'data/socofing/real'
-GENERIC_MODEL_URL = 'models/generic.pth'
-
-batch_size = 30 
-
-net = FingerprintModel()
-loss_fn = nn.MSELoss() 
-optimiser = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-trainset = DataLoader(Fingerprints('left index finger', random.randint(1, 600)), batch_size=batch_size, shuffle=True)
 # net.load_state_dict(torch.load(GENERIC_MODEL_URL))
-train(trainset, net, loss_fn, optimiser)
+train(1, trainset, net, loss_fn, optimiser)
 # torch.save(net.state_dict(), GENERIC_MODEL_URL)
