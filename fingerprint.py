92445c2e37d12b06fb1a1d2c6cff0c7e15898592
pslcn <gregorsrussellcobb@gmail.com>
2022-12-23 13:21:19 +0000

    Add feature extractor using VGG-16

--- fingerprint.py
+++ fingerprint.py
@@ -8,6 +8,7 @@ from torch import nn
 from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 import torch.nn.functional as F
+from torchvision import models
 
 class Fingerprints(Dataset):
     def __init__(self, finger, focus, img_dim=(416, 416)):
@@ -47,35 +48,37 @@ class ImageProcess:
         lambd = 10.0
         gamma = 0.5
         psi = 0
+
         for theta in np.arange(0, np.pi, np.pi / num_filters):
             kern = cv2.getGaborKernel((ksize, ksize), sigma, theta, lambd, gamma, psi, ktype=cv2.CV_64F)
             kern /= 1.0 * kern.sum()
             filters.append(kern)
+
         newimage = np.zeros_like(img)
         depth = -1
         for kern in filters:
             np.maximum(newimage, cv2.filter2D(img, depth, kern), newimage) 
+
         return newimage
 
-class FingerprintModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 3)
-        self.fc1 = nn.Linear(16 * 102 * 102, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 1)
-        self.s = nn.Sigmoid()
+class FeatureExtractor(nn.Module):
+    def __init__(self, model):
+        super(FeatureExtractor, self).__init__()
+
+        self.features = list(model.features)
+
+        self.features = nn.Sequential(
+                nn.Conv2d(1, 3, 3),
+                *self.features)
+        self.pooling = model.avgpool
+        self.fc = model.classifier[0]
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        x = self.s(x)
+        x = self.pooling(self.features(x))
+
+        x = torch.flatten(x, 0)
+        x = self.fc(x)
+
         return x
 
 def imshow(img):
@@ -86,30 +89,38 @@ def train(epochs, trainset):
     for e in range(epochs):
         for i, batch in enumerate(trainset):
             X, y = batch
+            X = torch.from_numpy(np.array(X)).float()
 
             # imshow(X[0][0].detach().numpy())
             # X = [ImageProcess.morph_op(img) for img in X]
             # X = [ImageProcess.gabor_filter(img) for img in X]
 
-            X = torch.from_numpy(np.array(X)).float()
+            features = extract_features(X)
 
-            optimiser.zero_grad()
-            pred = net(X.float())
-            loss = loss_fn(pred, y)
-            loss.backward()
-            optimiser.step()
-            print(f'epoch: {e + 1} batch: {i + 1} loss: {loss.item()}')
+            # optimiser.zero_grad()
+            # pred = net(X)
+            # loss = loss_fn(pred, y)
+            # loss.backward()
+            # optimiser.step()
+            # print(f'epoch: {e + 1} batch: {i + 1} loss: {loss.item()}')
+
+def extract_features(imgs):
+    with torch.no_grad():
+        features = [net(img).detach().numpy().reshape(-1) for img in imgs]
+    return np.array(features)
 
 DATASET_PATH = 'data/socofing/real'
 MODEL_PATH = 'models/'
 
-net = FingerprintModel()
-loss_fn = nn.MSELoss()
-optimiser = optim.Adam(net.parameters(), lr=0.01)
+model = models.vgg16(weights='DEFAULT')
+net = FeatureExtractor(model)
+
+# loss_fn = nn.MSELoss()
+# optimiser = optim.Adam(net.parameters(), lr=0.01)
 
 batch_size = 30
 
 train_fingerprints = Fingerprints('left index finger', random.randint(1, 600))
 train_focus = train_fingerprints.get_dataset_focus()
 
-train(4, DataLoader(train_fingerprints, batch_size=batch_size, shuffle=True))
+train(1, DataLoader(train_fingerprints, batch_size=batch_size, shuffle=True))
