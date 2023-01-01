bf807b5775e2a58e7030466c15b9bbe77117c488
pslcn <gregorsrussellcobb@gmail.com>
2023-01-01 12:50:39 +0000

    move model

--- fingerprint.py
+++ fingerprint.py
@@ -6,43 +6,9 @@ import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
-import torch.nn.functional as F
-from torchvision import models
 import imgs
 from imgs import Fingerprints
-
-class SimpleNet(nn.Module):
-    def __init__(self, focus):
-        super().__init__()
-        self.focus = focus
-        self.fe = nn.Sequential(
-            nn.Conv2d(1, 3, 3, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(3, 8, 3, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(8, 16, 3, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
-            nn.Flatten(1),
-            nn.Linear(16 * 52 * 52, 4096, bias=True)
-        )
-        self.conclude = nn.Sequential(
-            nn.Linear(4096, 1024),
-            nn.ReLU(inplace=True),
-            nn.Linear(1024, 84),
-            nn.ReLU(inplace=True),
-            nn.Linear(84, 1),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        x = self.fe(x)
-        focus = self.fe(self.focus)
-        x = torch.sub(x, focus)
-        x = self.conclude(x)
-        return x
+from model import SimpleNet
 
 def imshow(img):
     imgplot = plt.imshow(img.astype('uint8'), cmap='gray')
