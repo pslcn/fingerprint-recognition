991449680f109b5438edaed221540211023a39de
pslcn <gregorsrussellcobb@gmail.com>
2023-07-21 12:42:34 +0100

    refactor and clean up

--- fingerprint.py
+++ fingerprint.py
@@ -1,81 +1,47 @@
 import numpy as np
 import random
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-import torch
-from torch import nn
-from torch.utils.data import Dataset, DataLoader
-import torch.optim as optim
-import imgs
-from imgs import Fingerprints
-from model import SimpleCNN
-
-MODEL_PATH = 'models/'
-SAVE_PATH = MODEL_PATH + 'net.pth'
-
-# """ Training and Testing:
-DATASET_PATH = 'data/socofing/'
-TRAINSET_PATH = DATASET_PATH + 'real'
-TESTSET_PATH = lambda difficulty : DATASET_PATH + 'altered/' + difficulty
-
-def imshow(img):
-    imgplot = plt.imshow(img.astype('uint8'), cmap='gray')
-    plt.show()
-
-def train(epochs, trainset_path, finger, focus_id, focus, save_path, batch_size=30):
-    fingerprints = Fingerprints(trainset_path, finger, focus_id) 
-    fingerprints.pad_with_focus(focus)
-    net = SimpleCNN()
-    loss_fn = nn.MSELoss()
-    optimiser = optim.Adam(net.parameters(), lr=0.01)
-    for e in range(epochs):
-        for i, (X, y) in enumerate(DataLoader(fingerprints, batch_size=batch_size, shuffle=True)):
-            focus = torch.from_numpy(np.array(focus[random.randint(0, len(focus) - 1)])).float()[None]
-            # Preprocessing is done on X as np arrays
-            X = torch.from_numpy(np.array(X)).float() # Model uses tensors
-            optimiser.zero_grad()
-            pred = net(X, focus)
-            loss = loss_fn(pred, y)
-            loss.backward()
-            optimiser.step()
-            print(f'epoch: {e + 1} batch: {i + 1} loss: {loss.item()}')
-    torch.save(net.state_dict(), save_path)
-
-def test(testset_path, finger, focus_id, focus, load_path):
-    fingerprints = Fingerprints(testset_path, finger, focus_id)
-    fingerprints.pad_with_focus(focus)
-    net = SimpleCNN()
-    net.load_state_dict(torch.load(load_path))
-    batch_size = 30
-    net.eval()
-    with torch.no_grad():
-        for i, (X, y) in enumerate(DataLoader(fingerprints, batch_size=batch_size, shuffle=True)):
-            focus = torch.from_numpy(np.array(focus[random.randint(0, len(focus) - 1)])).float()[None]
-            X = torch.from_numpy(np.array(X)).float()
-            pred = net(X, focus)
-            num_correct = sum([1 if(abs(a - b) <= 0.1) else 0 for a, b in zip(pred.detach().numpy(), y.detach().numpy())])
-            print(f'batch: {i + 1} accuracy: {(num_correct / batch_size) * 100}%')
-
-FINGER = 'left index finger'.split(' ')
-FOCUS_ID = random.randint(1, 600)
-FOCUS = imgs.get_focus_fingerprints(TESTSET_PATH('hard'), FINGER, FOCUS_ID)
-
-# train(2, TRAINSET_PATH, FINGER, FOCUS_ID, FOCUS, SAVE_PATH)
-test(TESTSET_PATH('hard'), FINGER, FOCUS_ID, FOCUS, SAVE_PATH)
-# """
-
-"""
-import sys
+import glob
 import cv2
+import scipy
+import torch
 
-saved = cv2.resize(cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE), (416, 416))[np.newaxis, :, :]
-saved = torch.from_numpy(np.array(saved)).float()[None]
-query = cv2.resize(cv2.imread(sys.argv[2], cv2.IMREAD_GRAYSCALE), (416, 416))[np.newaxis, :, :]
-net = SimpleCNN()
-net.load_state_dict(torch.load(SAVE_PATH))
-net.eval()
-with torch.no_grad():
-    X = torch.from_numpy(np.array(query)).float()[None]
-    pred = net(X, saved)
-    print(f'pred: {pred.item()}')
-"""
+IMG_DIM = (416, 416)
+
+def fingerprint_paths(path, finger, focus_id):
+	return glob.glob(path + f"/*{finger[0].capitalize()}_{'_'.join([e for e in finger[1:]])}*.BMP")
+
+def img_is_focus(img_path, focus_id):
+	return int((img_path.split("/")[-1]).split("_")[0]) == focus_id
+
+def cv2_load_image_grayscale(img_path):
+	return cv2.resize(cv2.imread(img_path, cv2.IMREAD_GRAYSCALE), IMG_DIM)[np.newaxis, :, :]
+
+def get_focus_fingerprints(path, finger, focus_id):
+	focus_fingerprints = []
+	for img_path in fingerprint_paths(path, finger, focus_id):
+		if img_is_focus(img_path, focus_id):
+			focus_fingerprints.append(cv2_load_image_grayscale(img_path))
+	return focus_fingerprints
+
+class FingerprintsData:
+	def __init__(self, path, finger, focus_id):
+		self.labelled_data = []
+		for img_path in fingerprint_paths(path, finger, focus_id):
+			self.labelled_data.append((cv2_load_image_grayscale(img_path), torch.tensor([img_is_focus(img_path, focus_id)], dtype=torch.float64)))
+
+	def pad_with_focus(self, focus):
+		for o in range(len(self.labelled_data) // 4):
+			self.labelled_data.append((focus[random.randint(0, len(focus) - 1)], torch.tensor([1], dtype=torch.float64)))
+
+
+MODEL_PATH = "models/"
+SAVE_PATH = MODEL_PATH + "net.pth"
+
+DATASET_PATH = "data/socofing/"
+TRAINSET_PATH = DATASET_PATH + "real"
+TESTSET_PATH = lambda difficulty: DATASET_PATH + "altered/" + difficulty
+
+
+# FINGER = "left index finger".split(" ")
+# FOCUS_ID = random.randint(1, 600)
+# FOCUS = get_focus_fingerprints(TESTSET_PATH("hard"), FINGER, FOCUS_ID)
