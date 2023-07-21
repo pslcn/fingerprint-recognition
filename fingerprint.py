50096677210173090c8e0585a8d5208318000e74
pslcn <gregorsrussellcobb@gmail.com>
2023-07-21 13:13:07 +0100

    data parsing

--- fingerprint.py
+++ fingerprint.py
@@ -5,29 +5,25 @@ import cv2
 import scipy
 import torch
 
+import parse_dataset
+
 IMG_DIM = (416, 416)
-
-def fingerprint_paths(path, finger, focus_id):
-	return glob.glob(path + f"/*{finger[0].capitalize()}_{'_'.join([e for e in finger[1:]])}*.BMP")
-
-def img_is_focus(img_path, focus_id):
-	return int((img_path.split("/")[-1]).split("_")[0]) == focus_id
 
 def cv2_load_image_grayscale(img_path):
 	return cv2.resize(cv2.imread(img_path, cv2.IMREAD_GRAYSCALE), IMG_DIM)[np.newaxis, :, :]
 
 def get_focus_fingerprints(path, finger, focus_id):
 	focus_fingerprints = []
-	for img_path in fingerprint_paths(path, finger, focus_id):
-		if img_is_focus(img_path, focus_id):
+	for img_path in parse_dataset.fingerprint_paths(path, finger, focus_id):
+		if parse_dataset.img_is_focus(img_path, focus_id):
 			focus_fingerprints.append(cv2_load_image_grayscale(img_path))
 	return focus_fingerprints
 
 class FingerprintsData:
 	def __init__(self, path, finger, focus_id):
 		self.labelled_data = []
-		for img_path in fingerprint_paths(path, finger, focus_id):
-			self.labelled_data.append((cv2_load_image_grayscale(img_path), torch.tensor([img_is_focus(img_path, focus_id)], dtype=torch.float64)))
+		for img_path in parse_dataset.fingerprint_paths(path, finger, focus_id):
+			self.labelled_data.append((cv2_load_image_grayscale(img_path), torch.tensor([parse_dataset.img_is_focus(img_path, focus_id)], dtype=torch.float64)))
 
 	def pad_with_focus(self, focus):
 		for o in range(len(self.labelled_data) // 4):
